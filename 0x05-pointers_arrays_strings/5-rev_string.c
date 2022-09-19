c779c2906ce9080a689e0af0df8ec21c5f46ec3d
Joseph Keswet <jhezekiah19@gmail.com>
2022-09-19 21:14:30 +0000

    My commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,13 +5,11 @@
  * @s: input string
  * Return: string in reverse
  */
- 
 void rev_string(char *s)
 {
 char rev = s[0];
 int counter = 0;
-int i;
-        
+int i;       
 while (s[counter] != '\0')
 counter++;
 for (i = 0; i < counter; i++)
