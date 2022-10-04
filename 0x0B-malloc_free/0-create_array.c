56eaac2e12c0ed4cc3ce811372deeed162adf8d6
JosephKeswet <95522423+JosephKeswet@users.noreply.github.com>
2022-10-04 06:47:03 +0000

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*create_array - array for prints a string
-*@size: number elements array
-*@c: char
-*Return: pointer
+* create_array - array for prints a string
+* @size: number elements array
+* @c: char
+* Return: pointer
 */
 
 char *create_array(unsigned int size, char c)
@@ -23,7 +23,6 @@ if (buffer == 0)
 {
 return (NULL);
 }
-
 else
 {
 position = 0;
